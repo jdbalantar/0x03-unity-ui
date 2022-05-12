970365224be99fb824a4c428c41f5fb1f6fa2457
jdbalantar <jdbalantar@gmail.com>
2022-05-12 05:32:43 -0500

    Options Menu active Event

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,4 +10,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("maze");
     }
+    public void QuitMaze()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
 }
