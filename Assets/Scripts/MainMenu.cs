391d51f713b2902e932db1b5e1d3772c68aa85f8
jdbalantar <jdbalantar@gmail.com>
2022-05-12 05:46:36 -0500

    Adding ColorBlindMode to the Maze GameProject

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,6 +8,16 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
         SceneManager.LoadScene("maze");
     }
     public void QuitMaze()
@@ -15,4 +25,8 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
         Debug.Log("Quit Game");
     }
+
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
 }
